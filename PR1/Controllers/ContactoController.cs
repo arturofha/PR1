c48cba4465357b1d7fbea32c49d34d25ac3066d0
Arturo Herrera Aguilar <arturofha@hotmail.com>
2018-07-03 21:54:49 -0600

    Se agrega acción Get la cual recibe un id y devuelve un resultado

---
+++ PR1/Controllers/ContactoController.cs
@@ -0,0 +1,42 @@
+﻿using PR1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PR1.Controllers
+{
+    public class ContactoController : ApiController
+    {
+        Contacto[] contactos = new Contacto[]{
+          new Contacto() { Id=1,Nombre="Leonel", Apellido="Messi"} ,
+          new Contacto(){Id=2,Nombre="Cristiano",Apellido="Ronaldo"},
+          new Contacto(){Id=3,Nombre="Luis",Apellido="Suarez" },
+           new Contacto(){Id=3,Nombre="Andrés",Apellido="Iniesta" }
+        };
+
+        //Devolver una lista completa
+        public IEnumerable<Contacto> Get()
+        {
+            return contactos;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            // buscamos en la lista y obtenemos el primer id que encontremos
+            Contacto ct = contactos.FirstOrDefault<Contacto>(x => x.Id == id);
+
+            if(ct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ct);
+        }
+
+
+
+    }
+}
